ef7d2a7180ce793c019eb927e590945cb2e1012b
Ilya Konovalov <broken_67@bk.ru>
2024-02-06 20:10:23 +0300

    ver 1.0 - Добавлено: Ограничение скорости разгона, добавлен счетчик времени, 5 секундный таймер до начала гонки, удерживание машины до начала гонки.

---
+++ Assets/DriftCar/Scripts/RaceManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaceManager : MonoBehaviour
+{
+    public Transform startLine;
+    public Transform finishLine;
+    public Text raceStartText;
+    public Text timerText;
+    public GameObject carStop;
+
+    private bool raceStarted = false;
+    private float raceTime = 0f;
+
+    void Start()
+    {
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        raceStartText.text = "Заедз начнется через:  5";
+        yield return new WaitForSeconds(1f);
+        raceStartText.text = "Заедз начнется через:  4";
+        yield return new WaitForSeconds(1f);
+        raceStartText.text = "Заедз начнется через:  3";
+        yield return new WaitForSeconds(1f);
+        raceStartText.text = "Заедз начнется через:  2";
+        yield return new WaitForSeconds(1f);
+        raceStartText.text = "Заедз начнется через:  1";
+        yield return new WaitForSeconds(1f);
+        raceStartText.text = "Поехали!";
+        yield return new WaitForSeconds(1f);
+        raceStartText.text = "";
+        carStop.SetActive(false);
+        StartRace();
+    }
+
+
+    string FormatRaceTime(float timeInSeconds)
+    {
+        int minutes = Mathf.FloorToInt(timeInSeconds / 60);
+        int seconds = Mathf.FloorToInt(timeInSeconds % 60);
+        int milliseconds = Mathf.FloorToInt((timeInSeconds * 1000) % 1000);
+
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+    }
+
+
+
+    void Update()
+    {
+        if (raceStarted)
+        {
+            raceTime += Time.deltaTime;
+            timerText.text = "Время заезда: " + FormatRaceTime(raceTime);
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == finishLine)
+        {
+            FinishRace();
+        }
+    }
+
+    void StartRace()
+    {
+        raceStarted = true;
+    }
+
+    void FinishRace()
+    {
+        raceStarted = false;
+        timerText.text = "Заезд завершен - время: " + FormatRaceTime(raceTime);
+    }
+}
--- Assets/DriftCar/Scripts/SpeedCalculator.cs
+++ Assets/DriftCar/Scripts/SpeedCalculator.cs
@@ -8,6 +8,7 @@ public class SpeedCalculator : MonoBehaviour
 {
 
     public float Speed;
+    public float maxSpeed = 200f;
     public Rigidbody rb;
 
     public Text SpeedText;
@@ -18,8 +19,17 @@ public class SpeedCalculator : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 vel = rb.velocity;
-        
-        Speed = rb.velocity.magnitude * 3.6f; 
+
+        // Вычисление скорости в км/ч
+        float Speed = rb.velocity.magnitude * 3.6f;
+
+        // Проверяем, не превышает ли скорость максимальное значение
+        if (Speed > maxSpeed)
+        {
+            // Если скорость превышает максимальное значение, ограничиваем ее
+            rb.velocity = rb.velocity.normalized * (maxSpeed / 3.6f);
+            Speed = maxSpeed; // Обновляем значение скорости
+        }
 
         SpeedText.text = Speed.ToString("0");
     }
